e58e1abd850ae9f126aaf2e426deec36bde85914
Mateo Restrepo Higuita <36718406+marestrepohi@users.noreply.github.com>
2025-09-26 20:03:49 +0000

    construction

--- src/components/looker-studio-panel.tsx
+++ src/components/looker-studio-panel.tsx
@@ -4,38 +4,38 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const lookerStudioLinks = {
-    cobranzas: "https://lookerstudio.google.com/embed/reporting/5b21c42a-b61d-47cc-a108-cd021ebdcf47/page/yJkVF",
-    call: "https://lookerstudio.google.com/embed/reporting/ae94a5d8-c6a3-4b98-b358-7c158d063284/page/yJkVF"
+    servicios: "https://lookerstudio.google.com/u/0/reporting/ca0754f7-3807-4b4f-bb70-e2f7e6a04b08/page/uVhZF",
+    retencion: "https://lookerstudio.google.com/u/0/reporting/ca0754f7-3807-4b4f-bb70-e2f7e6a04b08/page/uVhZF"
 };
 
 export function LookerStudioPanel() {
   return (
-    <Tabs defaultValue="cobranzas" className="w-full">
+    <Tabs defaultValue="servicios" className="w-full">
         <div className="flex justify-center">
             <TabsList>
-                <TabsTrigger value="cobranzas">Casa Menor</TabsTrigger>
-                <TabsTrigger value="call">Call</TabsTrigger>
+                <TabsTrigger value="servicios">Servicios</TabsTrigger>
+                <TabsTrigger value="retencion">Retención</TabsTrigger>
             </TabsList>
         </div>
-        <TabsContent value="cobranzas">
+        <TabsContent value="servicios">
             <div className="w-full h-[70vh] border rounded-lg overflow-hidden mt-4">
                 <iframe
-                    title="Reporte Casa Menor"
+                    title="Reporte Servicios"
                     width="100%"
                     height="100%"
-                    src={lookerStudioLinks.cobranzas}
+                    src={lookerStudioLinks.servicios}
                     allowFullScreen
                     sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-forms allow-popups allow-downloads"
                 ></iframe>
             </div>
         </TabsContent>
-        <TabsContent value="call">
+        <TabsContent value="retencion">
             <div className="w-full h-[70vh] border rounded-lg overflow-hidden mt-4">
                 <iframe
-                    title="Reporte Call"
+                    title="Reporte Retención"
                     width="100%"
                     height="100%"
-                    src={lookerStudioLinks.call}
+                    src={lookerStudioLinks.retencion}
                     allowFullScreen
                     sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-forms allow-popups allow-downloads"
                 ></iframe>
